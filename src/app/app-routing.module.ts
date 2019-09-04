3b62fb5dc3571a93ce29eab6aa9ca00c3d248fd7
Bernhard Essl <bernhardessl@aeoontechnologies.com>
2019-09-04 09:56:59 +0200

    detail page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
   { path: 'list', loadChildren: './list/list.module#ListPageModule' },
-  { path: 'detail', loadChildren: './detail/detail.module#DetailPageModule' },
+  { path: 'detail/:exerciseId', loadChildren: './detail/detail.module#DetailPageModule' },
   { path: 'add-set', loadChildren: './add-set/add-set.module#AddSetPageModule' },
 ];
 
--- src/app/detail/detail.page.ts
+++ src/app/detail/detail.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {ExerciseService} from '../exercise.service';
+import {Exercise} from '../models';
 
 @Component({
   selector: 'app-detail',
@@ -6,10 +9,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./detail.page.scss'],
 })
 export class DetailPage implements OnInit {
+  exercise: Exercise;
 
-  constructor() { }
+  constructor(private activatedRoute: ActivatedRoute, private exerciseService: ExerciseService) { }
 
   ngOnInit() {
+    this.activatedRoute.paramMap.subscribe( p => {
+      const exerciseId = p.get('exerciseId');
+      this.exercise = this.exerciseService.getExercise(exerciseId);
+    });
   }
 
 }
