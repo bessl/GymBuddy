ee9c5f0df9b1ea3581c940f249faa4c3599853fc
Bernhard Essl <bernhardessl@aeoontechnologies.com>
2019-09-23 16:26:42 +0200

    api call for exercise detail

--- src/app/exercise.service.ts
+++ src/app/exercise.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Exercise } from './models';
 import { AngularFirestore } from '@angular/fire/firestore';
-import { map } from 'rxjs/operators';
+import {first, map} from 'rxjs/operators';
 import {HttpClient, HttpHeaders } from '@angular/common/http';
 import { GYMBUDDY_API_CONGIG } from './secrets';
 
@@ -10,6 +10,10 @@ import { GYMBUDDY_API_CONGIG } from './secrets';
 })
 export class ExerciseService {
 
+  private header = {
+    headers: new HttpHeaders().set('Authorization',  `Bearer ${GYMBUDDY_API_CONGIG.authToken}`)
+  };
+
   constructor(
     private angularFireStore: AngularFirestore,
     private http: HttpClient
@@ -17,31 +21,13 @@ export class ExerciseService {
   }
 
   getExercises(trainingsDay: string) {
-      const header = {
-          headers: new HttpHeaders().set('Authorization',  `Bearer ${GYMBUDDY_API_CONGIG.authToken}`)
-      };
-      this.http.get(
-        `${GYMBUDDY_API_CONGIG.url}/api/v1/exercises/by_day/${trainingsDay}`, header)
-    .subscribe(s => {
-        console.log(s);
-    });
-
-    return this.angularFireStore.collection<Exercise>(
-        'exercise', ref => ref.where('day', '==', +trainingsDay).orderBy('title'))
-        .snapshotChanges()
-      .pipe(
-        map(exercises => {
-          return exercises.map(
-            e => ({ id: e.payload.doc.id, ...e.payload.doc.data() })  // map document ID
-          );
-        })
-      );
+    return this.http.get(`${GYMBUDDY_API_CONGIG.url}/api/v1/exercises/by_day/${trainingsDay}`, this.header);
   }
 
   getExercise(exerciseID: string) {
-    return this.angularFireStore
-        .collection('exercise')
-        .doc(exerciseID)
-        .valueChanges();
+    return this.http.get(`${GYMBUDDY_API_CONGIG.url}/api/v1/exercises/${exerciseID}`, this.header)
+      .pipe(
+        map(array => array[0])  // get the first element of array
+      );
   }
 }
