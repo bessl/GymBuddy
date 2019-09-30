7437d61b3454e7bcc4d03e8c0471a528002cad00
Bernhard Essl <bernhardessl@aeoontechnologies.com>
2019-09-30 20:07:25 +0200

    save auth token to local storage

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,7 +1,7 @@
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { BehaviorSubject } from 'rxjs';
-import { GYMBUDDY_API_CONGIG } from './secrets';
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {BehaviorSubject} from 'rxjs';
+import {GYMBUDDY_API_CONGIG} from './secrets';
 
 @Injectable({
   providedIn: 'root'
@@ -13,16 +13,27 @@ export class ApiService {
   }
 
   refreshToken() {
-      this.http.post<{ access_token: string }>(GYMBUDDY_API_CONGIG.authUrl, {
-            client_id: GYMBUDDY_API_CONGIG.clientId,
-            client_secret: GYMBUDDY_API_CONGIG.clientSecret,
-            audience: GYMBUDDY_API_CONGIG.audience,
-            grant_type: GYMBUDDY_API_CONGIG.grantType
-          }
-          , {headers: new HttpHeaders().set('content-type', 'application/json')}
+    const timeNow = new Date().getTime();
+    const ApiLocaleStore = localStorage.getItem('apiAccessToken');
+
+    if (!ApiLocaleStore || JSON.parse(ApiLocaleStore).expiresAt < timeNow) {
+      this.http.post<{ access_token: string, expires_in: number }>(GYMBUDDY_API_CONGIG.authUrl, {
+        client_id: GYMBUDDY_API_CONGIG.clientId,
+        client_secret: GYMBUDDY_API_CONGIG.clientSecret,
+        audience: GYMBUDDY_API_CONGIG.audience,
+        grant_type: GYMBUDDY_API_CONGIG.grantType
+      }
+      , {headers: new HttpHeaders().set('content-type', 'application/json')}
       ).subscribe(d => {
+        localStorage.setItem('apiAccessToken', JSON.stringify({
+          expiresAt: timeNow + d.expires_in * 100,
+          accessToken: d.access_token
+        }));
         this.authToken.next(d.access_token);
       });
+    } else {
+      this.authToken.next(JSON.parse(ApiLocaleStore).accessToken);
+    }
   }
 
 }
